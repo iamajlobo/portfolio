f793c56cd55dacc6d4ef5817577035f364479fa5
iamAjLobo <aj.lobo.2004@gmail.com>
2026-01-28 17:13:44 +0800

    Fixed Link of Google Drive

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,10 +3,6 @@ import defaultImg from '../assets/placeholder.jpg';
 const ProjectCard = ({ project }) => {
   const { img, title, description, date, link } = project;
 
-  const handleOnclick = (drivePath) => {
-    window.open(drivePath,'_blank');
-  }
-
   return (
     <div className="group bg-white w-full md:max-w-60 rounded-sm overflow-hidden shadow-md hover:shadow-lg relative cursor-pointer">
       
@@ -24,23 +20,23 @@ const ProjectCard = ({ project }) => {
         <p className="text-gray-500 text-[12px] py-1 font-light">{date}</p>
       </div>
 
-      {/* Overlay */}
-      <div
-        onClick={()=>handleOnclick(link)}
-        className="
-          absolute inset-0
-          flex justify-center items-center
-          bg-black/10
-          opacity-0
-          group-hover:opacity-100
-          transition-opacity duration-300
-        "
-      >
-        <p className="text-white text-md font-semibold tracking-wider">
-          CLICK TO VIEW
-        </p>
-      </div>
-
+      <a
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="
+            absolute inset-0
+            flex justify-center items-center
+            bg-black/10
+            opacity-0
+            group-hover:opacity-100
+            transition-opacity duration-300
+          "
+        >
+          <p className="text-white text-md font-semibold tracking-wider">
+            CLICK TO VIEW
+          </p>
+        </a>
     </div>
   );
 };
